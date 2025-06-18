3e900d0037ae0147304b2232a24a3a736d0e9987
AleksanderGPL <me@aleksander.cc>
2025-06-18 16:20:15 -0700

    Add sign up from WantIt

---
+++ frontend/components/Ui/InputIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="bg-neutral-800 border border-neutral-600 transition-all rounded-l-lg px-3 flex items-center"
+  >
+    <span class="text-neutral-300 text-sm font-medium"><slot /></span>
+  </div>
+</template>
---
+++ frontend/pages/auth/sign-up.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <h1 class="text-xl font-semibold mb-4">Sign up</h1>
+    <form class="flex flex-col gap-2" @submit.prevent="signUp">
+      <div>
+        <UiLabel for="name">Name</UiLabel>
+        <UiInput
+          id="name"
+          v-model="name"
+          placeholder="John Doe..."
+          autocomplete="name"
+          class="w-full"
+        />
+      </div>
+      <div>
+        <UiLabel for="username">Username</UiLabel>
+        <div class="flex">
+          <UiInputIcon> @ </UiInputIcon>
+          <UiInput
+            id="username"
+            v-model="username"
+            placeholder="johndoe..."
+            autocomplete="username"
+            class="w-full rounded-l-none"
+          />
+        </div>
+      </div>
+      <div>
+        <UiLabel for="email">Email</UiLabel>
+        <UiInput
+          id="email"
+          v-model="email"
+          placeholder="john.doe@example.com..."
+          autocomplete="email"
+          class="w-full"
+        />
+      </div>
+      <div>
+        <UiLabel for="password">Password</UiLabel>
+        <UiInput
+          id="password"
+          v-model="password"
+          placeholder="Password..."
+          autocomplete="new-password"
+          type="password"
+          class="w-full"
+        />
+      </div>
+      <UiButton class="mt-2">Sign up</UiButton>
+      <p v-if="error" class="text-red-500 text-sm mt-2 text-center">
+        {{ error }}
+      </p>
+      <div class="flex flex-col">
+        <NuxtLink to="/" class="text-center">Back to Home</NuxtLink>
+        <NuxtLink to="/auth/sign-in" class="text-center"
+          >Have an account?</NuxtLink
+        >
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { signUpSchema } from '@/schema/services/auth';
+import { AxiosError } from 'axios';
+
+definePageMeta({
+  layout: 'auth'
+});
+
+const name = ref('');
+const username = ref('');
+const email = ref('');
+const password = ref('');
+const api = useApi();
+const userStore = useUserStore();
+const error = ref('');
+
+if (userStore.current) {
+  navigateTo('/');
+}
+
+async function signUp() {
+  try {
+    error.value = '';
+    const payload = {
+      name: name.value,
+      username: username.value,
+      email: email.value,
+      password: password.value
+    };
+
+    const validation = validate(signUpSchema, payload);
+
+    if (validation) {
+      error.value = validation;
+      return;
+    }
+
+    await api.post('/auth/register', payload);
+
+    navigateTo('/auth/sign-in');
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      error.value = e.response?.data.message;
+    } else {
+      error.value = 'Something went wrong';
+    }
+  }
+}
+</script>
